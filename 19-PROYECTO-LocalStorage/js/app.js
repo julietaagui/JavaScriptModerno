52385709c892d0c98c3258e53f781b853971d268
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-11-04 14:39:05 -0300

    filanizando proyecto de tweets LocalStorage

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -6,94 +6,107 @@ let tweets = [];
 eventListeners();
 
 // Eventos
-function eventListeners(){
-    //Cuando el ususario agrega un nuevo tweet
+function eventListeners() {
+    // Cuando el usuario agrega un nuevo tweet
     formulario.addEventListener('submit', aregagarTweet);
 
-    // cuando el documento esta listo
+    // Cuando el documento está listo
     document.addEventListener('DOMContentLoaded', () => {
+        // Cargar tweets desde localStorage
         tweets = JSON.parse(localStorage.getItem('tweets')) || [];
-        console.log(tweets);
-    })
-
-    console.log(tweets);
-
-    crearHTML();
+        crearHTML(); // Llama a crearHTML aquí para cargar los tweets
+    });
 }
 
-
 // Funciones
-function aregagarTweet(e){
+function aregagarTweet(e) {
     e.preventDefault();
     
     // Text area donde el usuario escribe
     const tweet = document.querySelector('#tweet').value;
     
-    //Validacion 
-    if(tweet.trim() === ''){
-        mostrarError('Un mensaje no puede ir vacio')
-        return; //evita que s eejecuten mas lineas d ecodigo
+    // Validación 
+    if (tweet.trim() === '') {
+        mostrarError('Un mensaje no puede ir vacío');
+        return; // Evita que se ejecuten más líneas de código
     }
 
     const tweetObj = {
         id: Date.now(),
         tweet
-    }
+    };
 
-    //Añadir al arreglo de tweets
+    // Añadir al arreglo de tweets
     tweets = [...tweets, tweetObj];
 
-    //Una vez creado vamos a agregar el html
+    // Una vez creado, vamos a agregar el html
     crearHTML();
 
     // Reiniciar formulario
     formulario.reset();
 }
 
-//Mostrar mensaje de error
-function mostrarError(error){
+// Mostrar mensaje de error
+function mostrarError(error) {
     const mensajeError = document.createElement('P');
     mensajeError.textContent = error;
-    mensajeError.classList.add('error')
+    mensajeError.classList.add('error');
 
-    //Insertarlo en el contenido
+    // Insertarlo en el contenido
     const contenido = document.querySelector('#contenido');
     contenido.appendChild(mensajeError);
 
-    //Elimina la alerta despues de 3seg
+    // Elimina la alerta después de 3 segundos
     setTimeout(() => {
         mensajeError.remove();
     }, 3000);
 }
 
-// Mustra un listado de los tweets
-function crearHTML(){
+// Muestra un listado de los tweets
+function crearHTML() {
     limpiarHTML();
 
-    if(tweets.length > 0){
-        tweets.forEach( tweet => {
-            // crear el html
+    if (tweets.length > 0) {
+        tweets.forEach(tweet => {
+
+            //Crear boton eliminar
+            const btnEliminar = document.createElement('a');
+            btnEliminar.classList.add('borrar-tweet');
+            btnEliminar.textContent = 'X';
+
+            //Añadir la funcion de eliminar
+            btnEliminar.onclick = () => {
+                borrarTweet(tweet.id);
+            }
+
+            // Crear el HTML
             const li = document.createElement('li');
             li.innerText = tweet.tweet;
 
-            // insertar en el html
-            listaTweers.appendChild(li)
+            //Asignar el boton
+            li.appendChild(btnEliminar)
+
+            // Insertar en el HTML
+            listaTweers.appendChild(li);
         });
     }
 
     sincronizarStorage();
 }
 
-// agrega los tweets actuales al localStorage
-function sincronizarStorage(){
-    localStorage.setItem('tweets', JSON.stringify(tweets))
+// Agrega los tweets actuales al localStorage
+function sincronizarStorage() {
+    localStorage.setItem('tweets', JSON.stringify(tweets));
 }
 
-//Limpiar html
-function limpiarHTML(){
-    while(listaTweers.firstChild){
+function borrarTweet(id){
+    tweets = tweets.filter( tweet => tweet.id !== id);
+    crearHTML();
+}
+
+// Limpiar HTML
+function limpiarHTML() {
+    while (listaTweers.firstChild) {
         listaTweers.removeChild(listaTweers.firstChild);
     }
 }
-
-
